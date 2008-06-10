48eea2f771739aebe37b17a92e6f8e9c4be28259
Marcos Augusto <x@nofxx.com>
2008-06-10 14:32:37 -0300

    fixes e traduz feio porem mais eficiente

--- scripts/traduz.rb
+++ scripts/traduz.rb
@@ -4,13 +4,20 @@ require 'rubygems'
 require 'hpricot'
 require 'open-uri'
 
+
 # #
 # BABYLON
 b = Hpricot(open("http://www.babylon.com/definition/#{ARGV[0]}/Portuguese")).at('#results-col').inner_text
-puts b.gsub("\n\n"," ").gsub("Abaixe este dicionário", "")
+puts b.gsub(/(\n\n)|(Erros Nunca Mais)|(Abaixe este dicionário)/, '')
+
+# #
+# URBAN DICTIONARY
+u = Hpricot(open("http://www.urbandictionary.com/define.php?term=#{ARGV[0]}")).at('#entries').inner_text
+puts u.gsub(/(\n\n)|(comments)/,'')
 
 # #
 # GOOGLE
+# http://www.wagnerandrade.com/blog/2008/05/ruby-snapshot-1
 html = Net::HTTP.new('translate.google.com').post('/translate_t', "langpair=en|pt&text=#{ARGV[0]}").body
 puts "\n\nGoogle Translate:"
 puts Hpricot(html).at('#result_box').inner_text
