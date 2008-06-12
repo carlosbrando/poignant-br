f22abd7e075ba527dece11c9c0858c1f1cc4ef69
Marcos Augusto <x@nofxx.com>
2008-06-12 12:17:37 -0300

    fixes e um traduz mais apresentavel

--- scripts/traduz.rb
+++ scripts/traduz.rb
@@ -4,14 +4,21 @@ require 'rubygems'
 require 'hpricot'
 require 'open-uri'
 
+puts '
+
 
 # #
 # BABYLON
+# # # # # # # # # # # # # # # # # # # # ###'
 b = Hpricot(open("http://www.babylon.com/definition/#{ARGV[0]}/Portuguese")).at('#results-col').inner_text
 puts b.gsub(/(\n\n)|(Erros Nunca Mais)|(Abaixe este dicionário)/, '')
 
+puts '
+
+
 # #
-# URBAN DICTIONARY
+# URBAN DICTONARY
+# # # # # # # # # # # # # # # # # # # # ###'
 begin
   u = Hpricot(open("http://www.urbandictionary.com/define.php?term=#{ARGV[0]}"))
   u = u.at('#entries').inner_text.gsub(/(\n\n)|(comments)/,'') 
@@ -20,11 +27,14 @@ rescue => e
   nil
 end
 
+puts '
+
+
 # #
 # GOOGLE
+# # # # # # # # # # # # # # # # # # # # ###'
 # http://www.wagnerandrade.com/blog/2008/05/ruby-snapshot-1
 html = Net::HTTP.new('translate.google.com').post('/translate_t', "langpair=en|pt&text=#{ARGV[0]}").body
-puts "\n\nGoogle Translate:"
 puts Hpricot(html).at('#result_box').inner_text
 
 # #
