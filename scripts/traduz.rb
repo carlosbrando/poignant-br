3d06e150c62903eee526fcdf20b52b644ef43260
Marcos Augusto <x@nofxx.com>
2008-06-12 12:24:36 -0300

    agora..agora...sim... falando nisso, sabiam q estamos bem proximos de terminar 1-50?

--- scripts/traduz.rb
+++ scripts/traduz.rb
@@ -4,31 +4,17 @@ require 'rubygems'
 require 'hpricot'
 require 'open-uri'
 
-puts '
-
-
-# #
-# BABYLON
-# # # # # # # # # # # # # # # # # # # # ###'
-b = Hpricot(open("http://www.babylon.com/definition/#{ARGV[0]}/Portuguese")).at('#results-col').inner_text
-puts b.gsub(/(\n\n)|(Erros Nunca Mais)|(Abaixe este dicionário)/, '')
-
-puts '
-
-
-# #
+puts '# #
 # URBAN DICTONARY
 # # # # # # # # # # # # # # # # # # # # ###'
 begin
   u = Hpricot(open("http://www.urbandictionary.com/define.php?term=#{ARGV[0]}"))
-  u = u.at('#entries').inner_text.gsub(/(\n\n)|(comments)/,'') 
-  puts u
+  puts u.at('#entries').inner_text.gsub(/(\n\n)|(comments)/,'') 
 rescue => e
   nil
 end
 
 puts '
-
 
 # #
 # GOOGLE
@@ -37,6 +23,13 @@ puts '
 html = Net::HTTP.new('translate.google.com').post('/translate_t', "langpair=en|pt&text=#{ARGV[0]}").body
 puts Hpricot(html).at('#result_box').inner_text
 
+puts '
+
+# #
+# BABYLON
+# # # # # # # # # # # # # # # # # # # # ###'
+b = Hpricot(open("http://www.babylon.com/definition/#{ARGV[0]}/Portuguese")).at('#results-col').inner_text
+puts b.gsub(/(\n\n)|(Erros Nunca Mais)|(Abaixe este dicionário)/, '')
 # #
 # BABEL FISH
 # TODO
